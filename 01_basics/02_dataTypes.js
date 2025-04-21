49da7c81ca39f9309a2211f2250ab3a273053c8b
Samarth Kalshetti <samarthkalshetti43@gmail.com>
2025-04-21 14:11:40 +0000

    learned and revised about datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat thye JS code as newer version
+
+//code should be readble
+
+
+//Primitive datatypes
+
+let name = "Sammy"; //String
+let age = 18; //Number (includes float, double and int in it)
+let isLoggedIn = false; //Boolean (true of false)
+
+let accBal = null;  //null => standalone value  but null is an object
+
+let bikeName; //undefined => If we only intialize datatype default value will be undefined. 
+
+//symbol => unique
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof accBal);
+console.log(typeof bikeName);
+
+
+ 
+//Non-primitive
+
+//Objects  
+//Arrays
